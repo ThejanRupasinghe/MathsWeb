fc9303637d6302fbd9663aaeeb7e8ecd5195b4a7
ThejanRupasinghe <thejan94@gmail.com>
2017-03-31 00:00:52 +0530

    added time tables

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,6 +6,7 @@
 
 	@include('home.partials.carousel')
 
+    @include('home.partials.testimonials')
 
 
 @endsection
--- resources/views/home/partials/testimonials.blade.php
+++ resources/views/home/partials/testimonials.blade.php
@@ -1,59 +1,43 @@
 
 <div class="testimonials">
     <div class="container">
-        <h3 class="head head1">C<span>අපේ  <i>දරුවන්ගේ</i> අදහස්</span></h3>
+        <h3 class="head head1"><span style="font-size: 50px"><i>Time Tables</i></span></h3>
         <ul id="flexiselDemo1">
             <li>
                 <div class="wthree_testimonials_grid_main">
                     <div class="wthree_testimonials_grid">
-                        <h4>Suspendisse sagittis nibh sit amet nisi imperdiet</h4>
-                        <p>Donec laoreet eu purus eu viverra. Vestibulum sed convallis massa,
-                            eu aliquet massa. Suspendisse lacinia rutrum tincidunt. Integer id erat porta,
-                            convallis tortor a, ullamcorper magna.</p>
-                        <div class="wthree_testimonials_grid_pos">
-                            <img src="images/2.png" alt=" " class="img-responsive"/>
-                        </div>
-                    </div>
-                    <div class="wthree_testimonials_grid1">
-                        <h5>Mark Henry</h5>
-                        <p>Teacher</p>
-                    </div>
-                </div>
-            </li>
-            <li>
-                <div class="wthree_testimonials_grid_main">
-                    <div class="wthree_testimonials_grid">
-                        <h4>Vestibulum sed convallis massa tincidunt</h4>
-                        <p>Donec laoreet eu purus eu viverra. Vestibulum sed convallis massa,
-                            eu aliquet massa. Suspendisse lacinia rutrum tincidunt. Integer id erat porta,
-                            convallis tortor a, ullamcorper magna.</p>
-                        <div class="wthree_testimonials_grid_pos">
-                            <img src="images/4.png" alt=" " class="img-responsive"/>
-                        </div>
-                    </div>
-                    <div class="wthree_testimonials_grid1">
-                        <h5>Linda Carl</h5>
-                        <p>Teacher</p>
+                        {{--<h4>Suspendisse sagittis nibh sit amet nisi imperdiet</h4>--}}
+                        {{--<p>Donec laoreet eu purus eu viverra. Vestibulum sed convallis massa,--}}
+                            {{--eu aliquet massa. Suspendisse lacinia rutrum tincidunt. Integer id erat porta,--}}
+                            {{--convallis tortor a, ullamcorper magna.</p>--}}
+                        {{--<div class="wthree_testimonials_grid_pos">--}}
+                            {{--<img src="images/2.png" alt=" " class="img-responsive"/>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                    {{--<div class="wthree_testimonials_grid1">--}}
+                        {{--<h5>Mark Henry</h5>--}}
+                        {{--<p>Teacher</p>--}}
+                        <img src="{{ URL::asset('images/time_table.jpg') }}" alt="First image" style="width: 660px; height: 600px">
                     </div>
                 </div>
             </li>
-            <li>
-                <div class="wthree_testimonials_grid_main">
-                    <div class="wthree_testimonials_grid">
-                        <h4>Integer id erat porta convallis tortor</h4>
-                        <p>Donec laoreet eu purus eu viverra. Vestibulum sed convallis massa,
-                            eu aliquet massa. Suspendisse lacinia rutrum tincidunt. Integer id erat porta,
-                            convallis tortor a, ullamcorper magna.</p>
-                        <div class="wthree_testimonials_grid_pos">
-                            <img src="images/3.png" alt=" " class="img-responsive"/>
-                        </div>
-                    </div>
-                    <div class="wthree_testimonials_grid1">
-                        <h5>Michael Paul</h5>
-                        <p>Teacher</p>
-                    </div>
-                </div>
-            </li>
+            {{--<li>--}}
+                {{--<div class="wthree_testimonials_grid_main">--}}
+                    {{--<div class="wthree_testimonials_grid">--}}
+                        {{--<h4>Vestibulum sed convallis massa tincidunt</h4>--}}
+                        {{--<p>Donec laoreet eu purus eu viverra. Vestibulum sed convallis massa,--}}
+                            {{--eu aliquet massa. Suspendisse lacinia rutrum tincidunt. Integer id erat porta,--}}
+                            {{--convallis tortor a, ullamcorper magna.</p>--}}
+                        {{--<div class="wthree_testimonials_grid_pos">--}}
+                            {{--<img src="images/4.png" alt=" " class="img-responsive"/>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                    {{--<div class="wthree_testimonials_grid1">--}}
+                        {{--<h5>Linda Carl</h5>--}}
+                        {{--<p>Teacher</p>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</li>--}}
         </ul>
         <script type="text/javascript">
             $(window).load(function () {
